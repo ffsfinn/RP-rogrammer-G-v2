d0458ea4d1dfc4ed414e8d7113d9915646d35bff
Finn <anthony.torres0926@gmail.com>
2020-01-10 23:05:18 -0600

    Tile Board Layout Completed

---
+++ src/component/Board/Board.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Squares from '../Squares/Squares'
+
+const Board = (props) => (
+    <div className='Board'>
+        
+        <Squares />
+        <Squares />
+        <Squares />
+        <Squares />
+        <Squares />
+
+    </div>
+);
+
+export default Board;
---
+++ src/component/Squares/Squares.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Squares = (props) => (
+
+<div class="square-container">
+
+  <div class="square">
+    <div class="content">
+    </div>
+  </div>
+
+  <div class="square">
+    <div class="content spread">
+    </div>
+  </div>
+
+  <div class="square">
+    <div class="content column">
+    </div>
+  </div>
+
+  <div class="square">
+    <div class="content spread">
+    </div>
+  </div>
+
+  <div class="square">
+    <div class="content column">
+    </div>
+  </div>
+
+</div>
+)
+
+export default Squares;
