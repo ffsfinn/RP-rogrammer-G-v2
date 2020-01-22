66e22bcedc032a21cd3411e116e2183851521d3e
Finn <anthony.torres0926@gmail.com>
2020-01-22 16:46:12 -0600

    Added Circle Component

---
+++ src/component/Circle/Circle.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Circle.css'
+
+const Circle = props => (
+    <div className='circle'/>
+)
+
+export default Circle
--- src/component/Square/Square.jsx
+++ src/component/Square/Square.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const Square = props => (
 
 <div class="square">
-    <div class="content">
-    </div>
+    <div class="content"></div>
 </div>
 
 )
